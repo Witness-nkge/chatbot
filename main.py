341fe2b7bf74533db120d127d581e5889f21d17b
witness nkge <79025245+Witness-nkge@users.noreply.github.com>
2022-06-17 22:46:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ question = st.text_input('input your question')
 if st.button('answer'):
   qna = pipeline("question-answering", model="distilbert-base-cased-distilled-squad")
   answer = qa(question = question,context = context)
-  st.write(f"Answer: '{answer['answer']} \nf"Confindence: {answer['score']}")
+  st.write(f"Answer: '{answer['answer']} \nConfindence: {answer['score']}")
