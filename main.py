5b1335ba28d47fae0f2e2650a7331183a7e2cfe1
witness nkge <79025245+Witness-nkge@users.noreply.github.com>
2022-06-17 22:39:56 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from transformers import pipeline
+
+st.title('AI Homework Helper')
+context = st.text_input('input context or paragraph you want to ask questions based on')
+question = st.text_input('input your question')
+
+if st.button('answer'):
+  qna = pipeline("question-answering", model="distilbert-base-cased-distilled-squad")
+  answer = qa(question = question,context = context)
+  st.write(f"Answer: '{answer['answer']} \nf"Confindence: {answer['score']}")
